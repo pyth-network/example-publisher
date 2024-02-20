9c7b310caf90973734dc0531836cfda8ff3dc315
Huy Ngo <huyngopt1994@gmail.com>
2024-02-20 18:05:10 +0700

    add new line

--- example_publisher/providers/ronin_katana.py
+++ example_publisher/providers/ronin_katana.py
@@ -22,7 +22,6 @@ class RoninKatana(Provider):
     def generate_header(self) -> dict:
         return {
             "Content-Type": "application/json",
-            "Authority": "api.roninchain.com",
             "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36"
         }
 
