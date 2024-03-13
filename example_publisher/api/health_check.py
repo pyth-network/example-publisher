f51c46e396faa1683238596bc04195746f908396
Keyvan <keyvan@dourolabs.xyz>
2024-03-13 10:22:11 -0700

    refactor: health check code to improve readability and performance

--- example_publisher/api/health_check.py
+++ example_publisher/api/health_check.py
@@ -8,29 +8,22 @@ from example_publisher.publisher import Publisher
 
 
 class HTTPRequestHandler(BaseHTTPRequestHandler):
-    publisher: Publisher = None
-    config: Config = None
-
-    def __init__(self, *args, **kwargs):
-        self.test_priod_secs: int = (
-            HTTPRequestHandler.config.health_check_test_period_secs
-        )
-        self.last_successful_update: float = (
-            HTTPRequestHandler.publisher.last_successful_update
-        )
-        super().__init__(*args, **kwargs)
+    publisher: Publisher
+    config: Config
 
     def is_healthy(self):
+        last_successful_update = HTTPRequestHandler.publisher.last_successful_update
         return (
-            self.last_successful_update is not None
-            and time.time() - self.last_successful_update < self.test_priod_secs
+            last_successful_update is not None
+            and time.time() - last_successful_update
+            < HTTPRequestHandler.config.health_check_test_period_secs
         )
 
     def do_GET(self):
         healthy = self.is_healthy()
         data = {
             "status": "ok" if healthy else "error",
-            "last_successful_update": self.last_successful_update,
+            "last_successful_update": HTTPRequestHandler.publisher.last_successful_update,
         }
 
         self.send_response(200 if healthy else 503)
