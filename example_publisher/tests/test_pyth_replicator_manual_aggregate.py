d6bd1ca1a3431231b0ef0b8f1261041d81740ce9
Keyvan <keyvan@dourolabs.xyz>
2024-03-18 14:01:16 -0700

    fix: all the type issues

--- example_publisher/tests/test_pyth_replicator_manual_aggregate.py
+++ example_publisher/tests/test_pyth_replicator_manual_aggregate.py
@@ -1,9 +1,10 @@
 import random
+from typing import List
 from example_publisher.providers.pyth_replicator import manual_aggregate
 
 
 def test_manual_aggregate_works():
-    prices = [1, 2, 3, 4, 5, 6, 8, 10, 12, 14]
+    prices: List[float] = [1, 2, 3, 4, 5, 6, 8, 10, 12, 14]
     random.shuffle(prices)
 
     agg_price, agg_confidence_interval = manual_aggregate(prices)
